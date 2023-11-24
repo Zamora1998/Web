256e67aa175d52e973cd73c84ed08c1cdd863a0b
Johnny Zamora <JohnnyZ@IT-JOHNNY-PC>
2023-11-24 17:15:09 -0600

    Muestra todas las imagnees descripcion y fecha

--- app/src/main/java/com/example/web/model/PeliculaAdapter.java
+++ app/src/main/java/com/example/web/model/PeliculaAdapter.java
@@ -9,7 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.web.R;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class PeliculaAdapter extends RecyclerView.Adapter<PeliculaAdapter.PeliculaViewHolder> {
 
@@ -30,13 +34,23 @@ public class PeliculaAdapter extends RecyclerView.Adapter<PeliculaAdapter.Pelicu
     public void onBindViewHolder(@NonNull PeliculaViewHolder holder, int position) {
         PeliculaModel pelicula = peliculas.get(position);
 
-        // Glide.with() se importa solo una vez
+        // Utiliza Glide para cargar la imagen
         Glide.with(holder.itemView.getContext())
                 .load(pelicula.getRutaPoster())
                 .into(holder.imageView);
 
-        holder.textView.setText(pelicula.getNombre());
+        // Configura los elementos de la vista
+        holder.textViewNombre.setText(pelicula.getNombre());
+
+        // Formatea la reseña y la fecha
+        String reseñaFormateada = "Reseña: " + pelicula.getResena();
+        String fechaOriginal = pelicula.getFechaLanzamiento();
+        String fechaFormateada = "Fecha: " + formatearFecha(fechaOriginal);
+
+        holder.textViewResena.setText(reseñaFormateada);
+        holder.textViewFecha.setText(fechaFormateada);
     }
+
 
     @Override
     public int getItemCount() {
@@ -45,12 +59,33 @@ public class PeliculaAdapter extends RecyclerView.Adapter<PeliculaAdapter.Pelicu
 
     public class PeliculaViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
-        TextView textView;
+        TextView textViewNombre;
+        TextView textViewResena;
+        TextView textViewFecha;
 
         public PeliculaViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.imageView);
-            textView = itemView.findViewById(R.id.textView);
+            textViewNombre = itemView.findViewById(R.id.textViewNombre);
+            textViewResena = itemView.findViewById(R.id.textViewResena);
+            textViewFecha = itemView.findViewById(R.id.textViewFecha);
+        }
+    }
+
+    private String formatearFecha(String fechaOriginal) {
+        try {
+            // Formato de entrada
+            SimpleDateFormat formatoOriginal = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
+
+            // Formato de salida
+            SimpleDateFormat formatoFormateado = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+
+            // Parsea la fecha original y la formatea
+            Date fecha = formatoOriginal.parse(fechaOriginal);
+            return formatoFormateado.format(fecha);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return fechaOriginal; // Devuelve la fecha original si hay un error
         }
     }
 }
